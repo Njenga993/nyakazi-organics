5c6c417ac36fd501509964a99a9e535ad453d8b4
Njengak993 <njengak993@gmail.com>
2026-04-13 11:47:06 +0200

    improvement in the shop folder added a new file

--- src/app/shop/[id]/generateStaticParams.ts
+++
@@ -1,8 +0,0 @@
-// src/app/shop/[id]/generateStaticParams.ts
-import { products } from "@/data/products";
-
-export async function generateStaticParams() {
-  return products.map((product) => ({
-    id: product.id.toString(),
-  }));
-}
