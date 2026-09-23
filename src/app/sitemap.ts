00eea9143ea171403fbc1df64a3511e4a0e5ad0c
Kelvin Kamau <njengak993@gmail.com>
2026-09-23 15:54:37 +0300

    added sitemap and robot

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://nyakaziorganics.org';
+  const baseUrl = 'https://www.nyakaziorganics.org';
 
   return [
     { url: baseUrl, lastModified: new Date(), changeFrequency: 'weekly', priority: 1 },
